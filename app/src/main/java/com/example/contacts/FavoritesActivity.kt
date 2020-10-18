cec3668261ba436c186a302cbae0e749fb90eb9f
Opadak <tfk1019@naver.com>
2020-10-18 21:20:30 +0900

    2020.10.18

---
+++ app/src/main/java/com/example/contacts/FavoritesActivity.kt
@@ -0,0 +1,14 @@
+package com.example.contacts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+
+class FavoritesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorites)
+
+    }
+}
+
