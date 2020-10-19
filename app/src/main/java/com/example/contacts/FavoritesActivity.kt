832b6dcae20e8de265570704c84712aa52e3fcaf
Opadak <tfk1019@naver.com>
2020-10-19 18:21:21 +0900

    2020.10.19

--- app/src/main/java/com/example/contacts/FavoritesActivity.kt
+++ app/src/main/java/com/example/contacts/FavoritesActivity.kt
@@ -1,13 +1,22 @@
 package com.example.contacts
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import kotlinx.android.synthetic.main.activity_favorites.*
 
 class FavoritesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favorites)
+
+
+        All_Contacts_intent2.setOnClickListener{
+            this.finish()
+
+
+        }
 
     }
 }
--- app/src/main/java/com/example/contacts/MainActivity.kt
+++ app/src/main/java/com/example/contacts/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.contacts
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.activity_favorites.*
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.zip.Inflater
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Favorites_intent.setOnClickListener{
+
+            val intent = Intent(this, FavoritesActivity::class.java)
+            startActivity(intent)
+
+        }   //intent
+
+        val personlist = ArrayList<String>() //Add View 리스트
+
+
+        val addview_container = findViewById<LinearLayout>(R.id.person_name)
+        val inflater = LayoutInflater.from(this)
+
+
     }
 }
